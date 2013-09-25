d1d18644ce229f1633acc23790542d79563945e8
Isaac Bythewood <isaac@bythewood.me>
2013-09-25 02:02:50 +0000

    Update pelicanconf.py settings to most recent pelican update

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,17 +15,28 @@ JINJA_EXTENSIONS = (
 TEMPLATE_PAGES = {
     'about.html': 'about/index.html',
 }
-FILES_TO_COPY = (
-    ('extra/CNAME', 'CNAME'),
-    ('extra/apple-touch-icon-114x114-precomposed.png', 'apple-touch-icon-114x114-precomposed.png'),
-    ('extra/apple-touch-icon-57x57-precomposed.png', 'apple-touch-icon-57x57-precomposed.png'),
-    ('extra/apple-touch-icon-72x72-precomposed.png', 'apple-touch-icon-72x72-precomposed.png'),
-    ('extra/apple-touch-icon.png', 'apple-touch-icon.png'),
-    ('extra/apple-touch-icon-precomposed.png', 'apple-touch-icon-precomposed.png'),
-    ('extra/favicon.ico', 'favicon.ico'),
-    ('extra/humans.txt', 'humans.txt'),
-    ('extra/robots.txt', 'robots.txt'),
+STATIC_PATHS = (
+    'extra/CNAME',
+    'extra/apple-touch-icon-114x114-precomposed.png',
+    'extra/apple-touch-icon-57x57-precomposed.png',
+    'extra/apple-touch-icon-72x72-precomposed.png',
+    'extra/apple-touch-icon.png',
+    'extra/apple-touch-icon-precomposed.png',
+    'extra/favicon.ico',
+    'extra/humans.txt',
+    'extra/robots.txt',
 )
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+    'extra/apple-touch-icon-114x114-precomposed.png': {'path': 'apple-touch-icon-114x114-precomposed.png'},
+    'extra/apple-touch-icon-57x57-precomposed.png': {'path': 'apple-touch-icon-57x57-precomposed.png'},
+    'extra/apple-touch-icon-72x72-precomposed.png': {'path': 'apple-touch-icon-72x72-precomposed.png'},
+    'extra/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'extra/apple-touch-icon-precomposed.png': {'path': 'apple-touch-icon-precompsed.png'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/humans.txt': {'path': 'humans.txt'},
+    'extra/robots.txt': {'path': 'robots.txt'},
+}
 
 
 LINKS = (
